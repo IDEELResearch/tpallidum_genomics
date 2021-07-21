3d8ea74c217441e06d5dd3be0dcd4183ebd15fb5
fnindo <fnindo@gmail.com>
2021-07-21 14:24:14 -0400

    Update tp_wgs_test1.py
    change of date

--- tp_wgs_test1.py
+++ tp_wgs_test1.py
@@ -1,6 +1,6 @@
 # Develop a strict pipeline to analysis Treponema pallidum Whome genome sequences data.
 # Author: Wentao Chen but adopted to run on unc longleaf cluster by Fredrick Nindo
-# Date : 2020/11/12
+# Date : 2021/07/21
 # NOTE : Install the Tp analysis environment first. More details : https://github.com/opplatek/bacterial_genome_analysis
 
 
